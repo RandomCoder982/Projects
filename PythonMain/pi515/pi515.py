5a5dde896ae71d5d5e316f91721052272f46bc16
RandomCoder982 <irenealobo12@gmail.com>
2024-02-29 14:10:57 -0600

    Updated

--- PythonMain/pi515/pi515.py
+++ PythonMain/pi515/pi515.py
@@ -2,39 +2,33 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
 import os
-from datetime import datetime, timezone
 import numpy as np
-from scipy import stats
 from matplotlib.animation import FuncAnimation
 
-# Load data
+# Load data. Needs data.csv to be in the same directory as file
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), "data.csv"))
 
                 
-#Function to change epochs to a more readable time
-def epoch2datetime(epoch):
-    v1 = (datetime.fromtimestamp(epoch, timezone.utc))
-    v2 = v1.strftime('%Y-%m-%d')
-    return v2
-
-#Applying that function to every value in epoch and creating a new columm containing those values
-dates = df["epoch"].apply(epoch2datetime)
+#Changing the dates from a Year-Month-Day value to an interger matplotlib can understand
 dates = (mdates.date2num(df["date"]))
 
 debug = True
 
 if debug:
-    #df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
+    #Creating the white background
     fig = plt.figure()
+
+    #Creating a 3d graph onto the background
     ax = fig.add_subplot(projection="3d")
+
+    #Graph and labeling the X, Y, and Z lists
     ax.scatter(df["distance"], [*range(len(dates))], dates[::-1])
     ax.set_xlabel('Distance')
     ax.set_ylabel('Calories')
     ax.set_zlabel('Dates')
+
+    #Changing the date's format back into Year-Month-Day
     ax.zaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    #plt.scatter(df["dates"], df["calories"])
-    #plt.scatter(df["calories"], df["dates"])
-    #plt.scatter([*range(len(df["dates"]))], df["calories"])
-    #plt.plot(df["dates"], df["calories"])
+
 
 plt.show()
