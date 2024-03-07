ee5cb1c939f163d4e329c6cc38a49de97f739c3a
RandomCoder982 <irenealobo12@gmail.com>
2024-03-07 11:36:45 -0600

    Don't know

--- PythonMain/graphs.py
+++ PythonMain/graphs.py
@@ -1,36 +1,57 @@
-from pyvis.network import Network
-import networkx as nx
-
-# nx_graph = nx.cycle_graph(10)
-# nx_graph.nodes[1]["title"] = "Number 1"
-
-# nt = Network()
-# nt.from_nx(nx_graph)
-# nt.show("nx.html")
-
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import matplotlib.dates as mdates
-from datetime import datetime
+import pandas as pd
+import os
+import numpy as np
+from matplotlib.animation import FuncAnimation
 
-# Sample data with dates
-dates = ['2024-01-01', '2024-01-02', '2024-01-03', '2024-01-04', '2024-01-05']
-values = [5, 7, 3, 8, 6]
+def best_fit(x,y):
 
-# Convert string dates to datetime objects
-date_objs = [datetime.strptime(date, '%Y-%m-%d') for date in dates]
+   slope, y_intercept = np.polyfit(x, y, deg=1)
+   slope = np.round(slope, 2)
+   y_intercept = np.round(y_intercept, 2)
 
-# Convert datetime objects to numeric values using date2num
-x = mdates.date2num(date_objs)
+   # Calculate R Squared
+   # 1 - RSS / TSS
+   r_squared = 1 - (sum((y - (slope * x + y_intercept))**2) / ((len(y) - 1) * np.var(y, ddof=1)))
+   r_squared = np.round(r_squared, 2)
+   
+   return slope, y_intercept, r_squared
 
-# Create figure and axis objects
-fig, ax = plt.subplots()
 
-# Create scatter plot using fig.scatter
-ax.scatter(x, values)
+# Load data. Needs data.csv to be in the same directory as file
+df = pd.read_csv(os.path.join(os.path.dirname(__file__), "data.csv"))
 
-# Format x-axis as dates
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+calories = [float(cal.replace(",", "")) for cal in df["calories"]]
 
-# Show the plot
+#Changing the dates from a Year-Month-Day value to an interger matplotlib can understand
+dates = (mdates.date2num(df["date"]))
+
+debug = True
+graph1 = False
+
+if debug:
+    if graph1:
+        #Creating the white background
+        fig = plt.figure()
+
+        #Creating a 3d graph onto the background
+        ax = fig.add_subplot(projection="3d")
+
+        #Graph and labeling the X, Y, and Z lists
+        ax.scatter(df["distance"], calories, dates)
+        ax.set_xlabel('Distance')
+        ax.set_ylabel('Calories')
+        ax.set_zlabel('Dates')
+
+        #Changing the date's format back into Year-Month-Day
+        ax.zaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
+    fig2, ax2 = plt.subplots()
+    slope1, y_int1 = best_fit(dates, calories)
+    xseq = np.linspace(0, len(dates), num=len(dates))
+    ax2.scatter(dates, calories)
+    ax2.plt(xseq, y_int1 + slope1 * xseq)
+
+
 plt.show()
-
--- PythonMain/pi515/pi515.py
+++ PythonMain/pi515/pi515.py
@@ -5,30 +5,53 @@ import os
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
+def best_fit(x,y):
+
+   slope, y_intercept = np.polyfit(x, y, deg=1)
+   slope = np.round(slope, 2)
+   y_intercept = np.round(y_intercept, 2)
+
+   # Calculate R Squared
+   # 1 - RSS / TSS
+   r_squared = 1 - (sum((y - (slope * x + y_intercept))**2) / ((len(y) - 1) * np.var(y, ddof=1)))
+   r_squared = np.round(r_squared, 2)
+   
+   return slope, y_intercept, r_squared
+
+
 # Load data. Needs data.csv to be in the same directory as file
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), "data.csv"))
 
-                
+calories = [float(cal.replace(",", "")) for cal in df["calories"]]
+
 #Changing the dates from a Year-Month-Day value to an interger matplotlib can understand
 dates = (mdates.date2num(df["date"]))
 
 debug = True
+graph1 = False
 
 if debug:
-    #Creating the white background
-    fig = plt.figure()
+    if graph1:
+        #Creating the white background
+        fig = plt.figure()
 
-    #Creating a 3d graph onto the background
-    ax = fig.add_subplot(projection="3d")
+        #Creating a 3d graph onto the background
+        ax = fig.add_subplot(projection="3d")
 
-    #Graph and labeling the X, Y, and Z lists
-    ax.scatter(df["distance"], [*range(len(dates))], dates[::-1])
-    ax.set_xlabel('Distance')
-    ax.set_ylabel('Calories')
-    ax.set_zlabel('Dates')
+        #Graph and labeling the X, Y, and Z lists
+        ax.scatter(df["distance"], calories, dates)
+        ax.set_xlabel('Distance')
+        ax.set_ylabel('Calories')
+        ax.set_zlabel('Dates')
 
-    #Changing the date's format back into Year-Month-Day
-    ax.zaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        #Changing the date's format back into Year-Month-Day
+        ax.zaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
+    fig2, ax2 = plt.subplots()
+    slope1, y_int1, rsqr = best_fit(dates, calories)
+    xseq = np.linspace(0, len(dates), num=len(dates))
+    ax2.scatter(dates, calories)
+    ax2.plot(xseq, y_int1 + slope1 * xseq)
 
 
 plt.show()
